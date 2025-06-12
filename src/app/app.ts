867ea5d56d7126f9ae617f5f7ff5ec7540b68abf
gabrielymorais <gabrielymoraisn.1@gmail.com>
2025-06-11 21:11:07 -0300

    feat: DarkMode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  imports: [RouterOutlet, NgClass]
 })
-export class App {
-  protected title = 'Receitas';
+export class App implements OnInit {
+  temaEscuro = false;
+
+  ngOnInit(): void {
+    const dark = localStorage.getItem('modo-escuro');
+    this.temaEscuro = dark === 'true';
+
+    if (this.temaEscuro) {
+      document.body.classList.add('dark-mode');
+    }
+  }
+
+  alternarTema() {
+    this.temaEscuro = !this.temaEscuro;
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('modo-escuro', this.temaEscuro.toString());
+  }
 }
